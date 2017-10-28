fca0f0934b64c13ddf8e5765a346a29a082a8231
bancheng1406410216 <954120326@qq.com>
2017-10-28 18:14:00 +0800

    2017:10:28:18:14:00

--- html5xm/js/01.js
+++ html5xm/js/01.js
@@ -26,3 +26,22 @@ map[3]=[0,0,0,0,0];
 map[4]=[0,0,0,0,0];
 var ys=[];
 ys[0]=["ffffff"];
+window.onload=function () {
+    //在页面加载的时候执行
+    var tx=0;
+    maxwidth=window.innerWidth/100*90;//在保证页面不会出现滚动条的情况下，使canvas的可以使用的范围最大
+    maxheight=window.innerHeight/100*90;
+    //上面的方法即可以在视网膜屏上使用，也可以在普通的电脑屏幕上使用。
+    //因为：如果通过css固定canvas的大小，那么：
+    //1.无法使canvas的大小自相应2.在视网膜屏上，canvas的实际像素大小是css的像素大小的2倍
+    tzfbl();
+    szcanvasdx();
+    hq();
+    yx();
+    if (localStorage.getItem("best")==null)
+    {
+        localStorage.setItem("best",0);
+    }
+    best=localStorage.getItem("best");
+    window.addEventListener('keydown', hqaj, true);
+}
