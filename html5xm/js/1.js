2cd2981b04c1fffc10ad425135ffa97801546339
tanchongyang1406410240 <1134233791@qq.com>
2017-10-28 17:32:04 +0800

    2017:10:28:17:32:04

--- html5xm/js/1.js
+++ html5xm/js/1.js
@@ -2,3 +2,10 @@ var bgmusic = document.getElementById('1mp3');
 bgmusic.addEventListener('canplay', function(){  
     this.play();  
 }, false);  
+window.addEventListener('load', function(){  
+    window.addEventListener('touchstart', once, false);  
+}, false);  
+function once(){  
+    bgmusic.play();  
+    window.removeEventListener('touchstart', once, false);  
+}  
