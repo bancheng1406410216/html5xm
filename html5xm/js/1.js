b3abc97660d189113b57d1fcd5b745d5d91a318e
tanchongyang1406410240 <1134233791@qq.com>
2017-10-28 17:33:04 +0800

    2017:10:28:17:33:04

--- html5xm/js/1.js
+++ html5xm/js/1.js
@@ -8,4 +8,18 @@ window.addEventListener('load', function(){
 function once(){  
     bgmusic.play();  
     window.removeEventListener('touchstart', once, false);  
-}  
+}
+var ctx;
+var mapdq=[];//存放地图数据
+var mapdqdx=[];//用来存放地图的大小
+var dqgs=0;//保存当前的关数
+var tph=20;
+var tps=20;
+var csh=0;
+var css=0;
+var rh=0;//玩家的当前位置
+var rs=0;//玩家的当前位置
+var ch=0;//迷宫的出口的位置
+var cs=0;//迷宫的出口的位置
+var wc=false;//是否完成了当前关卡
+var max=3;//与最大关数有关
