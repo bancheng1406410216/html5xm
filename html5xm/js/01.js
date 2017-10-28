dd6571d1b1e89bd445c91c6652b89e19e1470dbc
chenxi1406410219 <1134204415@qq.com>
2017-10-28 18:10:10 +0800

    2017:10:28:18:10:10

--- html5xm/js/01.js
+++ html5xm/js/01.js
@@ -2,3 +2,27 @@ var bgmusic = document.getElementById('2mp3');
 bgmusic.addEventListener('canplay', function(){  
     this.play();  
 }, false);
+window.addEventListener('load', function(){  
+    window.addEventListener('touchstart', once, false);  
+}, false);  
+function once(){  
+    bgmusic.play();  
+    window.removeEventListener('touchstart', once, false);  
+}
+var ctx;
+var maxwidth;//当前窗口的可以使用的大小
+var maxheight;//当前窗口的可以使用的大小
+var cshb;
+var best=0;//最好成绩
+var cj=0;//当前成绩
+var yzgz=0;//当前已有数字的格子的个数
+var dkd;
+var xkd;
+var map=[];
+map[0]=[0,0,0,0,0];
+map[1]=[0,0,0,0,0];
+map[2]=[0,0,0,0,0];
+map[3]=[0,0,0,0,0];
+map[4]=[0,0,0,0,0];
+var ys=[];
+ys[0]=["ffffff"];
