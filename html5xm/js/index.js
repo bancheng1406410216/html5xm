0ab57ab08aee355e116fed9029ea6aa95cd20beb
tanchongyang1406410240 <1134233791@qq.com>
2017-10-28 16:59:09 +0800

    2017:10:28:16:59:09

--- html5xm/js/index.js
+++ html5xm/js/index.js
@@ -1,3 +1,19 @@
         function yx() {
+            if (navigator.platform=="MacIntel")
+            {
+             if (window.devicePixelRatio=="2")
+            {
+            ;
+            }
+            else
+            {
+             alert("本网站建议在以下设备上浏览：拥有视网膜屏的imac或macbook pro或imac pro");
+            }
+            ;
+            }
+            else
+            {
+             alert("本网站建议在以下设备上浏览：拥有视网膜屏的imac或macbook pro或imac pro");
+            }
     }
     yx();
