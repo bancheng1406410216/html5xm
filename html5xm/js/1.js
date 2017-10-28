e69a89031a80d5aae30ee2fe3c08c4e2f1940174
tanchongyang1406410240 <1134233791@qq.com>
2017-10-28 17:46:21 +0800

    2017:10:28:17:46:21

--- html5xm/js/1.js
+++ html5xm/js/1.js
@@ -23,10 +23,16 @@ var ch=0;//迷宫的出口的位置
 var cs=0;//迷宫的出口的位置
 var wc=false;//是否完成了当前关卡
 var max=3;//与最大关数有关
+var kyky=0;
 window.onload=function () {
+    kyky=0;
     dqgs=0;
     szcanvasdx();
     hqhb();
+    if (kyky==1)
+    {
+        return;
+    }
     hmap();//绘制地图
     window.addEventListener('keydown', hqaj, true);
 }
@@ -43,6 +49,13 @@ function hqhb()
 {
     //获取canvas的js的对象
     var c=document.getElementById("myCanvas");
+    if (c.getContext){} 
+    else 
+    {
+        alert("此浏览器不支持html5的canvas元素。\n请更新浏览器版本到最新版或更换浏览器。\n推荐的浏览器为：\nchrome的最新版\n推荐的操作系统为：macOS High Sierra (10.13)\n");
+        return;
+    }
     ctx=c.getContext("2d"); 
+    kyky=1;
     alert("通过方向键使移动绿色的小球到红球的位置");  
 }
